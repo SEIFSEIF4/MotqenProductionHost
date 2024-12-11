e084754c05b4474f42c44167e933cb9337a700b0
hasanalkaf3 <hakalkaf482@gmail.com>
2024-12-11 19:38:17 +0300

    add DynamicBreadcrumb to about-motqen

--- app/[locale]/about-motqen/page.tsx
+++ app/[locale]/about-motqen/page.tsx
@@ -4,15 +4,18 @@ import { PageWrapper, SectionWrapper } from "@/components/Wrapper";
 import { SubTitle } from "@/components/ui/heading";
 import { useTranslations } from "next-intl";
 import AboutMotqen from "@/components/about/AboutMotqen";
+import DynamicBreadcrumb from "@/components/dynamicBreadcrumb";
 
 export default function HomePage() {
   const t = useTranslations("AboutMotqen");
 
   return (
-    <SectionWrapper id="about" className="rounded-lg bg-[#F3F4F6]">
-      <SubTitle text={t("title")} />
-
-      <AboutMotqen />
-    </SectionWrapper>
+    <PageWrapper>
+      <SectionWrapper id="about" className="rounded-lg bg-[#F3F4F6]">
+        <DynamicBreadcrumb />
+        <SubTitle text={t("title")} />
+        <AboutMotqen />
+      </SectionWrapper>
+    </PageWrapper>
   );
 }
