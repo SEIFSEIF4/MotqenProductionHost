d5b42ac1415d91e67a28dca07bc63e1962cc2058
hasanalkaf3 <hakalkaf482@gmail.com>
2024-12-23 19:16:03 +0300

    return footer style

--- components/SearchBox.tsx
+++ components/SearchBox.tsx
@@ -31,6 +31,7 @@ const SearchBox = ({
           setShowMobileMenu(false);
         }}
         className="relative items-center gap-2 rounded p-1 py-4 after:absolute after:bottom-0 after:left-1/2 after:h-[6px] after:w-[0%] after:-translate-x-1/2 after:rounded-full after:bg-[#6FA0A7] after:transition-all lg:flex lg:hover:after:w-[97%]"
+        aria-label="Search"
       >
         <Search isMobileMenu={false} />
         <span className="hidden lg:block">{title}</span>
