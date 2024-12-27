42402f0f14bea972db28db6662d62176a4443220
SEIFELESLLAM SEIF <seifsaif151@gmail.com>
2024-12-27 03:43:52 +0300

    hotfix: fix about page

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -45,62 +45,61 @@ function About() {
           />
         </div>
 
-          <div className="group relative col-span-7 row-span-2 h-full rounded-2xl p-6 text-white backdrop-blur-xl duration-300 hover:rounded-3xl md:col-span-3">
-            <div
-              className={`absolute inset-0 z-10 rounded-2xl from-[rgba(36,36,36,0.8)] to-[rgba(130,128,70,0.35)] duration-300 group-hover:rounded-3xl ltr:bg-gradient-to-l rtl:bg-gradient-to-r`}
-              aria-hidden="true"
-            />
-            <div className="absolute top-0 z-10 max-w-[50%] md:max-w-full ltr:right-0 ltr:-scale-x-100 rtl:left-0">
-              <AboutCarousel />
-            </div>
-            <div className="flex h-full w-full flex-col items-start justify-center gap-3 text-white">
-              <h2 className="z-20 mb-4 text-2xl font-bold">
-                {t("values.title")}
-              </h2>
-              <ul className="z-20 space-y-2 text-lg font-light">
-                <li>{t("values.value1")}</li>
-                <li>{t("values.value2")}</li>
-                <li>{t("values.value3")}</li>
-                <li>{t("values.value4")}</li>
-              </ul>
-            </div>
-            <Image
-              placeholder="blur"
-              quality={75}
-              src={Values}
-              alt="About Values"
-              fill
-              className="rounded-2xl"
-              sizes="(max-width: 640px) 80vw, (max-width: 1024px) 75vw, 50vw"
-              style={{ objectFit: "cover", objectPosition: "left" }}
-            />
+        <div className="group relative col-span-7 row-span-2 h-full rounded-2xl p-6 text-white backdrop-blur-xl duration-300 hover:rounded-3xl md:col-span-3">
+          <div
+            className={`absolute inset-0 z-10 rounded-2xl from-[rgba(36,36,36,0.8)] to-[rgba(130,128,70,0.35)] duration-300 group-hover:rounded-3xl ltr:bg-gradient-to-l rtl:bg-gradient-to-r`}
+            aria-hidden="true"
+          />
+          <div className="absolute top-0 z-10 max-w-[50%] md:max-w-full ltr:right-0 ltr:-scale-x-100 rtl:left-0">
+            <AboutCarousel />
           </div>
+          <div className="flex h-full w-full flex-col items-start justify-center gap-3 text-white">
+            <h2 className="z-20 mb-4 text-2xl font-bold">
+              {t("values.title")}
+            </h2>
+            <ul className="z-20 space-y-2 text-lg font-light">
+              <li>{t("values.value1")}</li>
+              <li>{t("values.value2")}</li>
+              <li>{t("values.value3")}</li>
+              <li>{t("values.value4")}</li>
+            </ul>
+          </div>
+          <Image
+            placeholder="blur"
+            quality={75}
+            src={Values}
+            alt="About Values"
+            fill
+            className="rounded-2xl"
+            sizes="(max-width: 640px) 80vw, (max-width: 1024px) 75vw, 50vw"
+            style={{ objectFit: "cover", objectPosition: "left" }}
+          />
+        </div>
 
-          <div className="group relative col-span-7 h-[186px] rounded-2xl p-6 text-white backdrop-blur-xl duration-300 hover:rounded-3xl">
-            <div
-              className="absolute inset-0 z-10 rounded-2xl bg-gradient-to-r from-[rgba(0,0,0,0.8)] to-[rgba(36,36,36,0.8)] duration-300 group-hover:rounded-3xl"
-              aria-hidden="true"
-            />
-            <div className="relative z-10 flex h-full flex-col justify-around">
-              <h2 className="mb-4 text-2xl font-bold">{t("mission.title")}</h2>
-              <p className="text-lg font-light md:w-3/4">
-                {t("mission.description")}
-              </p>
-            </div>
-            <Image
-              placeholder="blur"
-              quality={10}
-              src={Mission}
-              alt="About Mission"
-              fill
-              style={{ objectFit: "cover" }}
-              className="rounded-2xl"
-              sizes="(max-width: 640px) 80vw, (max-width: 1024px) 75vw, 50vw"
-            />
+        <div className="group relative col-span-7 h-[186px] rounded-2xl p-6 text-white backdrop-blur-xl duration-300 hover:rounded-3xl">
+          <div
+            className="absolute inset-0 z-10 rounded-2xl bg-gradient-to-r from-[rgba(0,0,0,0.8)] to-[rgba(36,36,36,0.8)] duration-300 group-hover:rounded-3xl"
+            aria-hidden="true"
+          />
+          <div className="relative z-10 flex h-full flex-col justify-around">
+            <h2 className="mb-4 text-2xl font-bold">{t("mission.title")}</h2>
+            <p className="text-lg font-light md:w-3/4">
+              {t("mission.description")}
+            </p>
           </div>
+          <Image
+            placeholder="blur"
+            quality={10}
+            src={Mission}
+            alt="About Mission"
+            fill
+            style={{ objectFit: "cover" }}
+            className="rounded-2xl"
+            sizes="(max-width: 640px) 80vw, (max-width: 1024px) 75vw, 50vw"
+          />
         </div>
       </div>
-    </section>
+    </SectionWrapper>
   );
 }
 
